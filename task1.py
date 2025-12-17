bc6469aa0aae24d1b9830177dd9b37618c4697a5
kamiliashenouda <kamilia.shenouda653@students.cmcss.net>
2025-12-17 14:09:17 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,22 +2,22 @@ total_slices =  party_pizza_mini + large + medium
 print(f"Total number of slices: {total_slices}")
 
 people += 1
-share = total_slices / / people
+share = total_slices // people
 leftover = total_slices % people
 print(f"Each person gets: {share}")
-print(f"Leftover slices: {Leftover}")
+print(f"Leftover slices: {leftover}")
 
 people +=1  #Eric and Brandon are coming too.
-share = total_slices / / people
+share = total_slices // people
 leftover = total_slices % people
 print(f"Each person gets: {share}")
-print(f"Leftover slices: {Leftover}")
+print(f"Leftover slices: {leftover}")
 
 #Mom says "Wait, Brandon’s coming. We’re going to need more pizza. I’ll upgrade the mini to a party_pizza instead. It’s the same as 2 minis. Hopefully the leftovers will be enough to fill his hollow leg.”
 
 people += 28
-share = total_slices / / people
+share = total_slices // people
 leftover = total_slices % people 
 print(f"Each person gets: {share}")
-print(f"Leftover slices: {Leftover}")
+print(f"Leftover slices: {leftover}")
 print("...for Mr. Hollow Leg")
